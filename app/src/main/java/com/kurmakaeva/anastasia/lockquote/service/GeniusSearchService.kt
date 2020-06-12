538d1fbebe0a83ecfd748d318276408f8cb2d9ae
Anastasia <stassiana@gmail.com>
2020-06-12 21:13:57 +0200

    Fixed unnecessary space in Task 2. Updated dependencies and versions. Added new credits.

---
+++ app/src/main/java/com/kurmakaeva/anastasia/lockquote/service/GeniusSearchService.kt
@@ -0,0 +1,44 @@
+package com.kurmakaeva.anastasia.lockquote.service
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import okhttp3.OkHttpClient
+
+private const val BASE_URL = "https://api.genius.com"
+
+val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+interface GeniusSearchService {
+    @GET("/search")
+
+    fun searchSongByTerm(@Query("q") q: String): Call<GeniusSearchResponse>
+
+    companion object {
+        val instance: GeniusSearchService by lazy {
+
+            val token = "fUXHqO4oK-lEh7wssQdKAskOe6m5SBv_K2wGvFg25FWowsEfEPgxSgsxl1RbXH2y"
+
+            val client = OkHttpClient.Builder().addInterceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(newRequest)
+            }.build()
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .baseUrl(BASE_URL)
+                .client(client)
+                .build()
+
+            retrofit.create<GeniusSearchService>(GeniusSearchService::class.java)
+        }
+    }
+}
